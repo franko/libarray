c44a5517a3a0eb5c16a10c162ea94a41842b5c22
Francesco Abbate <francesco.bbt@gmail.com>
2018-12-09 21:45:33 +0100

    Improve macros in array header

--- array.h
+++ array.h
@@ -7,40 +7,42 @@ struct generic_array {
     int length;
 };
 
-extern void generic_array_free(struct generic_array *a);
+#define array_method(type, name) array_ ## type ## _ ## name
+#define array_type_decl(type) \
+    struct array_ ## type { type* data; size_t size; int length; }; \
+    typedef struct array_ ## type array_ ## type[1]
 
+extern void generic_array_free(struct generic_array *a);
 extern void generic_array_ensure_size(struct generic_array *a, size_t element_size, int requested_size);
 
 #define declare_array(type) \
-    struct array_ ## type ## _struct { type* data; size_t size; int length; }; \
-    typedef struct array_ ## type ## _struct array_ ## type[1]; \
-    extern void array_ ## type ## _free(array_ ## type a); \
-    extern void array_ ## type ## _push(array_ ## type a, type v)
+    array_type_decl(type); \
+    extern void array_method(type, free)(array_ ## type a); \
+    extern void array_method(type, push)(array_ ## type a, type v)
 
 #define declare_array_inline(type) \
-    struct array_ ## type ## _struct { type* data; size_t size; int length; }; \
-    typedef struct array_ ## type ## _struct array_ ## type[1]; \
-    static void array_ ## type ## _free(array_ ## type a) { \
+    array_type_decl(type); \
+    static void array_method(type, free)(array_ ## type a) { \
         generic_array_free((struct generic_array *)a); \
     } \
-    static void array_ ## type ## _push(array_ ## type a, type v) { \
+    static void array_method(type, push)(array_ ## type a, type v) { \
         generic_array_ensure_size((struct generic_array *)a, sizeof(type), a->length + 1); \
         a->data[a->length] = v; \
         a->length ++; \
     }
 
 #define implement_array(type) \
-    void array_ ## type ## _free(array_ ## type a) { \
+    void array_method(type, free)(array_ ## type a) { \
         generic_array_free((struct generic_array *)a); \
     } \
-    void array_ ## type ## _push(array_ ## type a, type v) { \
+    void array_method(type, push)(array_ ## type a, type v) { \
         generic_array_ensure_size((struct generic_array *)a, sizeof(type), a->length + 1); \
         a->data[a->length] = v; \
         a->length ++; \
     }
 
 #define array(type) array_ ## type
-#define array_free(type) array_ ## type ## _free
-#define array_push(type) array_ ## type ## _push
+#define array_free(type) array_method(type, free)
+#define array_push(type) array_method(type, push)
 
 #define array_init() {{NULL, 0, 0 }}
